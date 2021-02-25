7ca292806d389baeb906a044f1368451cd0e676e
Arthur Mengual <arthur.mengual@gmail.com>
2021-02-25 16:02:19 +0100

    nouvel avancée du 25-02

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-#scrapper livre
+#scraper livre
 
 def scrapper_livre (url_book):
     
-    """
-    docstring: cette fonction donn ç en entrée et ça en sortie
-    """
+    
+    # Cette fonction prend en argument l'url de la page d'un livre du site books.toscrape.com et exrait les information relatives à ce livre contenues
+    # dans le code HTML de la page
+    
 
     response = requests.get(url_book)
 
@@ -21,8 +22,8 @@ def scrapper_livre (url_book):
         infos_book.append(url_book)
 
         #UPC
-        td = soup.find_all('td')
-        upc = td[0].text
+        balise_td = soup.find_all('td')
+        upc = balise_td[0].text
         infos_book.append(upc)
         
         #title
@@ -31,16 +32,16 @@ def scrapper_livre (url_book):
         infos_book.append(title.text)
 
         #price with tax
-        price_with_tax = td[3]
+        price_with_tax = balise_td[3]
         infos_book.append(price_with_tax.text)
 
         #price without tax
 
-        price_without_tax = td[2]
+        price_without_tax = balise_td[2]
         infos_book.append(price_without_tax.text)
 
         #number_available
-        number_available = td[5]
+        number_available = balise_td[5]
         infos_book.append(number_available.text)
 
         #product_description
@@ -53,25 +54,31 @@ def scrapper_livre (url_book):
         infos_book.append(category)
 
         #review_rating
-        review_rating = td[6]
+        review_rating = balise_td[6]
         infos_book.append(review_rating.text)
 
 
         #image_url
-        image_url = soup.find('img')
-        infos_book.append(image_url)
+        balise_img = soup.find('img')
+        image_url_incomplete = balise_img.get('src')
+        image_url_complete = image_url_incomplete.replace('../..', 'http://books.toscrape.com')
+        infos_book.append(image_url_complete)
 
         print(infos_book)
 
-#scrapper catégorie
+
+#scraper catégorie
 
 def scrapper_category(url_category):
+
+    # Cette fonction prend en argument l'url de la page d'une catégorie de livre du site 'books.toscrape.com' et exrait les url de tous les livres
+    #de la catégorie. Puis elle applique la fonction scrapper_livre à chaque url pour extraire les informations relatives à tous les livres de cette catégorie
 
     response_category = requests.get(url_category)
 
     soup_category = BeautifulSoup(response_category.text, 'lxml')
 
-    #faire une boucle applicant le fn scrapper lire à chaque url de categorie
+    #faire une boucle applicant la fn scrapper lire à chaque url de categorie
 
     for article in soup_category.find_all('article'):
         a = article.find_all('a')[0]
@@ -80,10 +87,9 @@ def scrapper_category(url_category):
         scrapper_livre(url_complete_each_book)
 
 
-    #exporter résultat dans un .csv
-
-
 def csv():
+
+    # exporter résultat dans .csv
 
     with open('category.csv', 'w', newline='') as csvfile:
         
@@ -97,9 +103,14 @@ def csv():
             thewriter.writerow({''})
 
 
+#scraper site
+
 if __name__ == 'main':
 
-    #scrapper site
+    #les lignes de codes ci après permettent d'extraire les url de chaque catégorie de livre du site 'books.tocrape.com' et d'appliquer la fonction
+    #scraper_catégorie qui elle même applique à chaque url de livre la fonction scraper_livre. Le résultat étant d'extraire toutes les informations relatives
+    #à tous les livres de toutes les catégories du site.
+
 
     url_site = 'https://books.toscrape.com/index.html'
 
@@ -107,19 +118,16 @@ if __name__ == 'main':
 
     soup_site = BeautifulSoup(response_site.text, 'lxml')
 
-    lis = soup_site.find_all('li')[2]
+    balise_li = soup_site.find_all('li')[2]
 
-    for li in lis.find_all('li'):
-        a = li.find('a')
-        url_incomplete_categories = a.get('href')
+    for li in balise_li.find_all('li'):
+        link = li.find('a')
+        url_incomplete_categories = link.get('href')
         url_complete_categories = "http://books.toscrape.com/" + url_incomplete_categories
         scrapper_category(url_complete_categories)
 
-    
+scrapper_livre('https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html')
 
 
-
-# snake case
 # variable de 3 lettres, exemple a devient link
-# faire dico au lieu des listes
-
+# faire dico au lieu des liste
